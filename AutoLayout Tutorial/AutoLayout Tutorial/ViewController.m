1c99c0db2843b00ca43a04d4038bd0ba9f07bc9d
Paul <paul.huynh3@gmail.com>
2017-09-17 23:07:17 -0400

    finish constraints

--- AutoLayout Tutorial/AutoLayout Tutorial/ViewController.m
+++ AutoLayout Tutorial/AutoLayout Tutorial/ViewController.m
@@ -20,10 +20,14 @@
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)buttonTapped:(UIButton *)sender {
+    
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"X"]) {
+        [sender setTitle:@"A very long title for this button"
+                forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"X" forState:UIControlStateNormal];
+    }
 }
-
 
 @end
